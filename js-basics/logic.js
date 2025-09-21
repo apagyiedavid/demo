31067d7c40d9aade4ab6ec8f9aa62e5edeeef23a
apagyiedavid <apagyiek@gmail.com>
2025-09-21 15:29:19 +0000

    js-basics

---
+++ js-basics/logic.js
@@ -0,0 +1,8 @@
+let number = 5;
+if (number > 0) {
+    console.log(number + " is a positive number.");
+} else if (number < 0) {
+    console.log(number + " is a negative number.");
+} else {
+    console.log("The number is zero.");
+}
---
+++ js-basics/lop.js
@@ -0,0 +1,8 @@
+for (let i = 1; i<= 10; i++) {
+    console.log(i);
+}
+function sum(num1, num2) {
+    return num1 + num2;
+}
+let result = sum(5, 7);
+console.log("the sum is: " + result);
